fa73aff13d2cd0496321029ad174f4394b72f21f
n7v <novsem@gmail.com>
2023-03-06 01:46:37 +0200

    code for experiments

---
+++ experiments/real.sh
@@ -0,0 +1,33 @@
+cd ..
+make
+sage trees_generation.sage 5 13 17 --gm-add=100 > d8_re_trees.log 2>&1
+sage testrelations.sage > experiments/d8_re_rels.log 2>&1
+sage dlog_cyc_PoC.sage > experiments/d8_re_dlog_1.log 2>&1
+sage dlog_cyc_PoC.sage > experiments/d8_re_dlog_2.log 2>&1
+sage dlog_cyc_PoC.sage > experiments/d8_re_dlog_3.log 2>&1
+sage dlog_cyc_PoC.sage > experiments/d8_re_dlog_4.log 2>&1
+sage dlog_cyc_PoC.sage > experiments/d8_re_dlog_5.log 2>&1
+
+sage trees_generation.sage 5 13 17 29 --gm-add=100 > d16_re_trees.log 2>&1
+sage testrelations.sage > experiments/d16_re_rels.log 2>&1
+sage dlog_cyc_PoC.sage > experiments/d16_re_dlog_1.log 2>&1
+sage dlog_cyc_PoC.sage > experiments/d16_re_dlog_2.log 2>&1
+sage dlog_cyc_PoC.sage > experiments/d16_re_dlog_3.log 2>&1
+sage dlog_cyc_PoC.sage > experiments/d16_re_dlog_4.log 2>&1
+sage dlog_cyc_PoC.sage > experiments/d16_re_dlog_5.log 2>&1
+
+sage trees_generation.sage 5 13 17 29 37 --gm-add=100 > d32_re_trees.log 2>&1
+sage testrelations.sage > experiments/d32_re_rels.log 2>&1
+sage dlog_cyc_PoC.sage > experiments/d32_re_dlog_1.log 2>&1
+sage dlog_cyc_PoC.sage > experiments/d32_re_dlog_2.log 2>&1
+sage dlog_cyc_PoC.sage > experiments/d32_re_dlog_3.log 2>&1
+sage dlog_cyc_PoC.sage > experiments/d32_re_dlog_4.log 2>&1
+sage dlog_cyc_PoC.sage > experiments/d32_re_dlog_5.log 2>&1
+
+sage trees_generation.sage 5 13 17 29 37 41 --gm-add=100 > d64_re_trees.log 2>&1
+sage testrelations.sage > experiments/d64_re_rels.log 2>&1
+sage dlog_cyc_PoC.sage > experiments/d64_re_dlog_1.log 2>&1
+sage dlog_cyc_PoC.sage > experiments/d64_re_dlog_2.log 2>&1
+sage dlog_cyc_PoC.sage > experiments/d64_re_dlog_3.log 2>&1
+sage dlog_cyc_PoC.sage > experiments/d64_re_dlog_4.log 2>&1
+sage dlog_cyc_PoC.sage > experiments/d64_re_dlog_5.log 2>&1
