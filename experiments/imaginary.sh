3bf20074a9971912c9dc345758d10cd2fbebabf6
n7v <novsem@gmail.com>
2023-03-06 02:09:02 +0200

    experiments and new release

--- experiments/imaginary.sh
+++ experiments/imaginary.sh
@@ -1,6 +1,6 @@
 cd ..
 make
-sage trees_generation.sage -3 -7 -11 --gm-add=100 > d8_im_trees.log 2>&1
+sage trees_generation.sage -3 -7 -11 --gm-add=100 > experiments/d8_im_trees.log 2>&1
 sage testrelations.sage > experiments/d8_im_rels.log 2>&1
 sage dlog_cyc_PoC.sage > experiments/d8_im_dlog_1.log 2>&1
 sage dlog_cyc_PoC.sage > experiments/d8_im_dlog_2.log 2>&1
@@ -8,7 +8,7 @@ sage dlog_cyc_PoC.sage > experiments/d8_im_dlog_3.log 2>&1
 sage dlog_cyc_PoC.sage > experiments/d8_im_dlog_4.log 2>&1
 sage dlog_cyc_PoC.sage > experiments/d8_im_dlog_5.log 2>&1
 
-sage trees_generation.sage -3 -7 -11 -19 --gm-add=100 > d16_im_trees.log 2>&1
+sage trees_generation.sage -3 -7 -11 -19 --gm-add=100 > experiments/d16_im_trees.log 2>&1
 sage testrelations.sage > experiments/d16_im_rels.log 2>&1
 sage dlog_cyc_PoC.sage > experiments/d16_im_dlog_1.log 2>&1
 sage dlog_cyc_PoC.sage > experiments/d16_im_dlog_2.log 2>&1
@@ -16,7 +16,7 @@ sage dlog_cyc_PoC.sage > experiments/d16_im_dlog_3.log 2>&1
 sage dlog_cyc_PoC.sage > experiments/d16_im_dlog_4.log 2>&1
 sage dlog_cyc_PoC.sage > experiments/d16_im_dlog_5.log 2>&1
 
-sage trees_generation.sage -3 -7 -11 -19 -23 --gm-add=100 > d32_im_trees.log 2>&1
+sage trees_generation.sage -3 -7 -11 -19 -23 --gm-add=100 > experiments/d32_im_trees.log 2>&1
 sage testrelations.sage > experiments/d32_im_rels.log 2>&1
 sage dlog_cyc_PoC.sage > experiments/d32_im_dlog_1.log 2>&1
 sage dlog_cyc_PoC.sage > experiments/d32_im_dlog_2.log 2>&1
@@ -24,7 +24,7 @@ sage dlog_cyc_PoC.sage > experiments/d32_im_dlog_3.log 2>&1
 sage dlog_cyc_PoC.sage > experiments/d32_im_dlog_4.log 2>&1
 sage dlog_cyc_PoC.sage > experiments/d32_im_dlog_5.log 2>&1
 
-sage trees_generation.sage -3 -7 -11 -19 -23 -31 --gm-add=100 > d64_im_trees.log 2>&1
+sage trees_generation.sage -3 -7 -11 -19 -23 -31 --gm-add=100 > experiments/d64_im_trees.log 2>&1
 sage testrelations.sage > experiments/d64_im_rels.log 2>&1
 sage dlog_cyc_PoC.sage > experiments/d64_im_dlog_1.log 2>&1
 sage dlog_cyc_PoC.sage > experiments/d64_im_dlog_2.log 2>&1
--- experiments/real.sh
+++ experiments/real.sh
@@ -1,6 +1,6 @@
 cd ..
 make
-sage trees_generation.sage 5 13 17 --gm-add=100 > d8_re_trees.log 2>&1
+sage trees_generation.sage 5 13 17 --gm-add=100 > experiments/d8_re_trees.log 2>&1
 sage testrelations.sage > experiments/d8_re_rels.log 2>&1
 sage dlog_cyc_PoC.sage > experiments/d8_re_dlog_1.log 2>&1
 sage dlog_cyc_PoC.sage > experiments/d8_re_dlog_2.log 2>&1
@@ -8,7 +8,7 @@ sage dlog_cyc_PoC.sage > experiments/d8_re_dlog_3.log 2>&1
 sage dlog_cyc_PoC.sage > experiments/d8_re_dlog_4.log 2>&1
 sage dlog_cyc_PoC.sage > experiments/d8_re_dlog_5.log 2>&1
 
-sage trees_generation.sage 5 13 17 29 --gm-add=100 > d16_re_trees.log 2>&1
+sage trees_generation.sage 5 13 17 29 --gm-add=100 > experiments/d16_re_trees.log 2>&1
 sage testrelations.sage > experiments/d16_re_rels.log 2>&1
 sage dlog_cyc_PoC.sage > experiments/d16_re_dlog_1.log 2>&1
 sage dlog_cyc_PoC.sage > experiments/d16_re_dlog_2.log 2>&1
@@ -16,7 +16,7 @@ sage dlog_cyc_PoC.sage > experiments/d16_re_dlog_3.log 2>&1
 sage dlog_cyc_PoC.sage > experiments/d16_re_dlog_4.log 2>&1
 sage dlog_cyc_PoC.sage > experiments/d16_re_dlog_5.log 2>&1
 
-sage trees_generation.sage 5 13 17 29 37 --gm-add=100 > d32_re_trees.log 2>&1
+sage trees_generation.sage 5 13 17 29 37 --gm-add=100 > experiments/d32_re_trees.log 2>&1
 sage testrelations.sage > experiments/d32_re_rels.log 2>&1
 sage dlog_cyc_PoC.sage > experiments/d32_re_dlog_1.log 2>&1
 sage dlog_cyc_PoC.sage > experiments/d32_re_dlog_2.log 2>&1
@@ -24,7 +24,7 @@ sage dlog_cyc_PoC.sage > experiments/d32_re_dlog_3.log 2>&1
 sage dlog_cyc_PoC.sage > experiments/d32_re_dlog_4.log 2>&1
 sage dlog_cyc_PoC.sage > experiments/d32_re_dlog_5.log 2>&1
 
-sage trees_generation.sage 5 13 17 29 37 41 --gm-add=100 > d64_re_trees.log 2>&1
+sage trees_generation.sage 5 13 17 29 37 41 --gm-add=100 > experiments/d64_re_trees.log 2>&1
 sage testrelations.sage > experiments/d64_re_rels.log 2>&1
 sage dlog_cyc_PoC.sage > experiments/d64_re_dlog_1.log 2>&1
 sage dlog_cyc_PoC.sage > experiments/d64_re_dlog_2.log 2>&1
